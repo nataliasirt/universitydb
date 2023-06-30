c8b4f8545a0b685fe53ab63fb1d956f4e734db41
nataliasirt <nsirtakjob@gmail.com>
2023-06-30 09:30:42 -0500

    updates as of 06.30.23

--- src/main/java/com/solvd/university/service/impl/jackson/IGroupService.java
+++ src/main/java/com/solvd/university/service/impl/jackson/IGroupService.java
@@ -5,8 +5,8 @@ import com.solvd.university.models.Group;
 import java.util.List;
 
 public interface IGroupService {
-    public Group select(int id);
-    public List<Group> selectAll();
-    public void update (Group group, int id);
-    public void delete (Group group, int id);
+    Group select(int id);
+    List<Group> selectAll();
+    void update (Group group, int id);
+    void delete (Group group, int id);
 }
